0fa85fc535040f97d884b6e69c75b0785803e6c7
Janilson Varela <janilsonjvs@gmail.com>
2023-08-02 14:04:15 -0300

    feat: field widget com bootstrap

---
+++ core/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+
+
+class TextInputBootstrap(forms.TextInput):
+    def __init__(self, attrs=None, render_value=False):
+        attrs = {'class': 'form-control'}
+        super().__init__(attrs)
+
+
+class SelectBootStrap(forms.Select):
+    def __init__(self, attrs=None, render_value=False):
+        attrs = {'class': 'form-select'}
+        super().__init__(attrs)
+
+
+class NumberInputBootstap(forms.NumberInput):
+    def __init__(self, attrs=None, render_value=False):
+        attrs = {'class': 'form-control'}
+        super().__init__(attrs)
+
+
+class CheckboxInputBootstrap(forms.CheckboxInput):
+    def __init__(self, attrs=None, render_value=False):
+        attrs = {'class': 'form-check-input'}
+        super().__init__(attrs)
