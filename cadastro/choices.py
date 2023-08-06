4a3eb39fdfcb080d92b7cc633e782f097c05c55c
Janilson Varela <janilsonjvs@gmail.com>
2023-08-06 17:44:24 -0300

    feat: choices, tipointervalo e tipo visibilidade

--- cadastro/choices.py
+++ cadastro/choices.py
@@ -6,3 +6,16 @@ class TipoPagamento(models.IntegerChoices):
     AVISTA = 1, _('A Vista')
     PRAZO = 2, _('A Prazo')
 
+
+class TipoIntervalo(models.TextChoices):
+    DIARIO = 'D', _('Diario')
+    SEMANAL = 'S', _('Semanal')
+    MENSAL = 'M', _('Mensal')
+    ANUAL = 'A', _('Anual')
+
+
+class TipoVisibilidade(models.IntegerChoices):
+    PEDIDO = 1, _('Pedido')
+    COMPRA = 2, _('Compra')
+    AMBOS = 3, _('Ambos')
+    
