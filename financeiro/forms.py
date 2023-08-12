938a07c6147e8c321eddc4331ee170fe773a853c
Janilson Varela <janilsonjvs@gmail.com>
2023-08-11 23:06:12 -0300

    feat: modalform contareceber

--- financeiro/forms.py
+++ financeiro/forms.py
@@ -3,10 +3,23 @@ from core.forms import *
 from financeiro.models import ContaReceber
 
 class ContaReceberForm(forms.ModelForm):
+    # data_vencimento = forms.DateField(
+    #     widget=forms.DateInput(format='%Y-%m-%d'),
+
+    #     #input_formats=('%m-%d-%Y', )
+    #  )
+
+
     class Meta:
         model = ContaReceber
-        fields = ['codigo', 'nome']
+        fields = ['documento', 'parcela', 'contato', 'data_emissao', 'data_vencimento', 'valor_titulo', 'observacao', 'situacao']
         widgets = {
-            'codigo': TextInputBootstrap(),
-            'nome': TextInputBootstrap(),
+            'documento': TextInputBootstrap(),
+            'parcela': NumberInputBootstap(),
+            'contato': SelectBootstrap(),
+            'data_emissao': DateInputBootstrap(format='%Y-%m-%d'),
+            'data_vencimento': DateInputBootstrap(format='%Y-%m-%d'),
+            'valor_titulo': NumberInputBootstap(),
+            'observacao': TextInputBootstrap(),
+            'situacao': SelectBootstrap()
         }
