27eb7b7fab732ffba367332eaab76495adc67ff4
Janilson Varela <janilsonjvs@gmail.com>
2023-08-19 20:43:30 -0300

    feat: ajuste no timeout alert

--- cadastro/static/js/cadastro.js
+++ cadastro/static/js/cadastro.js
@@ -5,7 +5,7 @@ django.jQuery().ready(function(){
         django.jQuery(".alert").fadeOut("slow", function(){
             $(this).alert("close");
         });
-    },3000);
+    },4000);
 });
 
 /**
