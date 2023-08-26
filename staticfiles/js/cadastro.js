a2e8c47fc07d2ddba563d3871bc5e7bfdcbca99a
Janilson Varela <janilsonjvs@gmail.com>
2023-08-25 21:15:54 -0300

    feat: collectstatic

---
+++ staticfiles/js/cadastro.js
@@ -0,0 +1,28 @@
+django.jQuery().ready(function(){
+    removeErrorElementos();
+
+    setTimeout(function(){
+        django.jQuery(".alert").fadeOut("slow", function(){
+            $(this).alert("close");
+        });
+    },4000);
+});
+
+/**
+ * Remove os erros dos elementos caso 
+ * o usuario preencha com algum valor
+ */
+function removeErrorElementos() {
+
+    django.jQuery("input, select").each((_, e)=> {
+        django.jQuery(e).change((el) => {
+            if (django.jQuery(el.target).hasClass('is-invalid'))
+            {
+                if (django.jQuery(el.target).val() != "" ) {
+                    django.jQuery(el.target).removeClass('is-invalid');
+                    django.jQuery(el.target).parent().find('.errorlist').remove();
+                }
+            }
+        });
+    });
+}
