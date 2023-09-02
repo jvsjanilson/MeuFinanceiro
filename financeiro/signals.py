336dec8beaf9d4aefdc5354a517dd50e6da81057
Janilson Varela <janilsonjvs@gmail.com>
2023-09-02 11:14:38 -0300

    feat: atualizacao apos a baixa

--- financeiro/signals.py
+++ financeiro/signals.py
@@ -1,4 +1,4 @@
-from financeiro.models import BaixaReceber, ContaReceber
+from financeiro.models import BaixaReceber, ContaReceber, BaixaPagar, ContaPagar
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from django.db.models import Sum
@@ -13,6 +13,20 @@ def pre_save_baixareceber(sender, instance, **kwargs):
 
     obj = ContaReceber.objects.get(pk=instance.contareceber.id)
     if baixas + instance.valor_pago >= instance.contareceber.valor_titulo:
+        obj.situacao = SituacaoFinanceiro.PAGO_TOTAL
+    else:
+        obj.situacao = SituacaoFinanceiro.PAGO_PARCIAL
+    obj.save()
+
+
+@receiver(pre_save, sender=BaixaPagar)
+def pre_save_baixapagar(sender, instance, **kwargs):
+    baixas = BaixaPagar.objects.filter(contapagar=instance.contapagar.id).aggregate(total=Sum('valor_pago'))['total']
+    if baixas is None: 
+        baixas = 0
+
+    obj = ContaPagar.objects.get(pk=instance.contapagar.id)
+    if baixas + instance.valor_pago >= instance.contapagar.valor_titulo:
         obj.situacao = SituacaoFinanceiro.PAGO_TOTAL
     else:
         obj.situacao = SituacaoFinanceiro.PAGO_PARCIAL
