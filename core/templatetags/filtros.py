91edf4756367fe33c7201e3f8450a500eb5b7053
Janilson Varela <janilsonjvs@gmail.com>
2023-09-14 10:34:52 -0300

    feat: ajuste no novo templatetag

--- core/templatetags/filtros.py
+++ core/templatetags/filtros.py
@@ -15,7 +15,12 @@ def pagination_filtros(request, page_number):
 
 @register.simple_tag
 def page_current(request):
+    queries = request.GET.copy()
     page_number = request.GET.get('page', 1)
-    if page_number == 1:
-        return ""
-    return f'?page={page_number}'
+    queries.pop('page', None)
+    querystring = f'?page={page_number}'
+
+    for q in queries.items():
+        querystring += f"&{q[0]}={q[1]}"
+
+    return querystring
